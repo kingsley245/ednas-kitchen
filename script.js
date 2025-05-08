1cc096f4348024f84b1d06dc48963c6a1ed35f9c
Oyeins-GUI <oyeinx7@gmail.com>
2025-05-08 18:10:39 +0100

    initiail commit

--- script.js
+++ script.js
@@ -1,88 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function (){
-//   const togglebutton = document.querySelector('.navbar__mobile-menu-toggle');
-
-//   const mobilemenu = document.querySelector('.navbar__mobile-menu-items');
-
-//   togglebutton.addEventListener('click', function (){
-//     mobilemenu.classList.toggle('active');
-//   });
-
-// })
-
-// console.log('kingsley')
-
-
-// window.addEventListener('scroll', function() {
-//   const navbar = this.document.querySelector('.navbar');
-
-//   if (window.scrollY > 0 ){
-//     navbar.classList.add('navbar--scroll');
-
-//   }else {
-//     navbar.classList.remove('navbar--scroll');
-//   }
-
-// })
-
-
-
-
-
-
-// const hero = document.getElementById('hero');
-
-
-// let index = 0;
-// const images = [
-//   `.background\ image-1.jpg`,
-//   `.background\ image-2.jpg`,
-//   `./images/background\ image-3.jpg`,
-//   `./images/background\ image-4.jpg`,
-// ];
-
-// function changeBackground(){
-//   hero.style.backgroundImage = `url(${images[index]})`;
-//   index = (index + 1) % images.length;
-// }
-
-
-// setInterval(changeBackground,3000)
-
-// changeBackground();
-
-
-
-
-
-
-
-
-
-
-
-
-// const images = [
-//   `url((images/background\ image-1.jpg))`,
-//   `url((images/background\ image-2.jpg))`,
-//   `url((images/background\ image-3.jpg))`,
-//   `url((.images/background\ image-4.jpg))`,
-// ];
-
-// let index = 0;
-// setInterval (() => {
-//   hero.style.backgroundImage = images[index];
-//   index = (index + 1) % images.length;
-// },3000);
-
-
-
-// function showSidebar (){
-//   const Sidebar = document.querySelector('.navbar__mobile-menu-list')
-//   Sidebar.style.display = 'flex'
-
-  
-// }
-
 document.querySelector('.menu-toggle').addEventListener('click', function (){
   document.getElementById('menu').classList.add('open');
   const menutoggle = document.querySelector('.menu-toggle');
@@ -119,16 +34,6 @@ handleScreenResize();
 window.addEventListener('resize', handleScreenResize);
 
 const removemenu = document.getElementById('menu')
-
-// function remove_menu (){
- 
-// }
-
-// remove_menu();
-
-// console.log(window.innerWidth);
-// console.log(screen.width)
-
 window.addEventListener('scroll', function() {
   const navbar = this.document.querySelector('.navbar');
 
